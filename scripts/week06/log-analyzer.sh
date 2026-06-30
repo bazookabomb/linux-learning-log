3c13cd0e4edda223256f9a35fc39414f38ab8575
bazookabomb <riedel79mirko@gmail.com>
2026-06-30 14:41:56 +0000

    log-analyzer V1.1

--- scripts/week06/log-analyzer.sh
+++ scripts/week06/log-analyzer.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-echo "Linux Log Analyzer V1.0"
+echo "Linux Log Analyzer V1.1"
+echo
 echo "Datum: $(date)"
 echo "Hostname: $(hostname)"
+echo
 echo "Fehlgeschlagene Systemdienste"
 systemctl --failed --no-pager
+echo
 echo "Fehler seit dem letzten Boot"
 journalctl -b -p err --no-pager
+echo
 echo "Warnungen seit dem letzten Boot"
 journalctl -b -p warning --no-pager
+echo
+echo "Kernel-Fehler"
+journalctl -k -p warning --no-pager
+echo
+echo "Out of Memory Fehler"
+journalctl --no-pager | grep -i "out of memory"
+
